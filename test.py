8811ef2c3d752b29fb6376afb2aff5819b360218
NoeZM <noe.zm2000@gmail.com>
2024-07-01 00:51:00 -0600

    se agrega modulo de salia de pensionados desde TPV y barrera

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-from Views.ViewPanelConfig import View_Panel_Config
-from Views.test_pantalla_entrada import Entrada
-from Views.test_pantalla_salida import Salida
+# se tienen que comentar las llineas que no se usarán o dará error
+
+# from Views.ViewPanelConfig import View_Panel_Config
+# from Views.test_pantalla_entrada import Entrada
+# from Views.test_pantalla_salida import Salida
+from Views.ViewCobro import ViewCobro
 
 
+ViewCobro()
 # Entrada()
 # Salida()
 # View_Panel_Config()
